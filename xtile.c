e9e45bb9bfe927c30317c8c80a2cf36264e32617
dhrxvxd-arch <dhrxvxd@gmail.com>
2026-04-26 17:48:56 +0530

    minor

--- xtile.c
+++ xtile.c
@@ -67,6 +67,7 @@ enum { CurNormal, CurResize, CurMove, CurLast };
 static const unsigned short borderwidth = 2;
 static unsigned long scheme[2][3];
 static const char *termcmd[] = {"st", NULL};
+static const char *roficmd[] = {"rofi", "-show", "run", NULL};
 
 static const char col_1[] = "#222222";
 static const char col_2[] = "#444444";
@@ -118,16 +119,17 @@ static void tag(const union Key *k);
 static struct Client *getclient(Window w);
 
 static struct KeyGr keys[] = {
-    {MODMASK, XK_Return, spawn, {.v = termcmd}},
-    {MODMASK, XK_q, quit, {0}},
-    {MODMASK, XK_w, killclient, {0}},
-    {MODMASK, XK_j, focusnext, {0}},
-    {MODMASK, XK_1, view, {.ui = 1 << 0}},
-    {MODMASK, XK_2, view, {.ui = 1 << 1}},
-    {MODMASK, XK_3, view, {.ui = 1 << 2}},
-    {MODMASK | ShiftMask, XK_1, tag, {.ui = 1 << 0}},
-    {MODMASK | ShiftMask, XK_2, tag, {.ui = 1 << 1}},
-    {MODMASK | ShiftMask, XK_3, tag, {.ui = 1 << 2}},
+    {MODMASK, 			XK_Return, 	spawn, 		{.v = termcmd}},
+    {MODMASK, 			XK_space,  	spawn, 		{.v = roficmd}},
+    {MODMASK, 			XK_q, 		quit, 		{0}},
+    {MODMASK, 			XK_w, 		killclient, 	{0}},
+    {MODMASK, 			XK_j, 		focusnext, 	{0}},
+    {MODMASK, 			XK_1, 		view, 		{.ui = 1 << 0}},
+    {MODMASK, 			XK_2, 		view, 		{.ui = 1 << 1}},
+    {MODMASK, 			XK_3, 		view, 		{.ui = 1 << 2}},
+    {MODMASK|ShiftMask, 	XK_1, 		tag, 		{.ui = 1 << 0}},
+    {MODMASK|ShiftMask, 	XK_2, 		tag, 		{.ui = 1 << 1}},
+    {MODMASK|ShiftMask, 	XK_3, 		tag, 		{.ui = 1 << 2}},
 };
 
 static void view(const union Key *k) {
